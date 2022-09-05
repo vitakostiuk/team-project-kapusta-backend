569674afb32b5e9253e452956d68e86fc6ad3472
zpengineer <clickprime94@gmail.com>
2022-09-05 21:13:49 +0300

    Refactor get and add transactions services

--- schemas/transactions/addTransSchema.js
+++ schemas/transactions/addTransSchema.js
@@ -1,22 +1,42 @@
 const Joi = require('joi');
 
-const customTransactionValidation = (value, helpers) => {
+const customDateValidation = (value, helpers) => {
+    if (Number(value) > 31) return helpers.message("Day must be a valid");
+}
+
+const customMonthValidation = (value, helpers) => {
+    if (Number(value) > 12) return helpers.message("Month must be a valid");
+}
+
+const customYearValidation = (value, helpers) => {
+    if (value.length < 4 || value.length > 4) return helpers.message("Year must be a valid");
+}
+
+const customValueValidation = (value, helpers) => {
     if (typeof value !== 'number') return helpers.message("Value must be a number");
 
     if (value.toString().length >= 11) return helpers.message("Value length should not exceed 9 numbers");
     
     if (Math.sign(value) === -1) return helpers.message("Value must be positive");
+
+    if (value === 0) return helpers.message("Value must be must greater than 0");
+}
+
+const customDescriptionValidation = (value, helpers) => {
+    const regEx = /^[A-Za-z0-9а-яА-ЯёЁ_ -]*$/;
+
+    if(!regEx.test(value)) return helpers.message("Description must not contain special characters");
 }
 
 const addTransSchema = Joi.object({
     date: Joi.object({
-        day: Joi.string(),
-        month: Joi.string(),
-        year: Joi.string()
+        day: Joi.string().custom(customDateValidation, 'custom day validation'),
+        month: Joi.string().custom(customMonthValidation, 'custom month validation'),
+        year: Joi.string().custom(customYearValidation, 'custom month validation')
     }),
-    description: Joi.string().min(3).max(51).required(),
-    categories: Joi.string(),
-    value: Joi.number().custom(customTransactionValidation, 'custom validation transaction').required(),
+    description: Joi.string().min(3).max(51).custom(customDescriptionValidation).required(),
+    categories: Joi.string().required(),
+    value: Joi.number().custom(customValueValidation, 'custom value validation').required(),
     income: Joi.boolean()
 })
 
--- services/transaction/addTransaction.js
+++ services/transaction/addTransaction.js
@@ -5,36 +5,51 @@ const addTransaction = tryCatchWrapper(async ({ _id, body, type }) => {
   const { balance } = await User.findById(_id);
   const { value, categories: idCategory } = body;
 
-  let newBalance, income, calculatedValue;
+  const {title, type: typeCat} = await Category.findById(idCategory);
+
+  let newBalance, income, calculatedValue, titleCat;
   const valueToNumber = Number(value);
   const floatValue = valueToNumber.toFixed(2);
 
-  switch (type) {
-    case "income":
-      calculatedValue = balance + Number(floatValue);
-      newBalance = calculatedValue.toFixed(2);
-      income = true;
-      break;
-    case "expense":
-      calculatedValue = balance - Number(floatValue);
-      newBalance = calculatedValue.toFixed(2);
-      income = false;
-      break;
-    default:
-      return;
+  // switch (type) {
+  //   case "income":
+  //     calculatedValue = balance + Number(floatValue);
+  //     newBalance = calculatedValue.toFixed(2);
+  //     income = true;
+  //     break;
+  //   case "expense":
+  //     calculatedValue = balance - Number(floatValue);
+  //     newBalance = calculatedValue.toFixed(2);
+  //     income = false;
+  //     break;
+  //   default:
+  //     return;
+  // }
+
+  if (type === 'income' && typeCat === 'income') {
+    calculatedValue = balance + Number(floatValue);
+    newBalance = calculatedValue.toFixed(2);
+    income = true;
+    titleCat = title;
+  } else if (type === 'expense' && typeCat === 'expenses') {
+    calculatedValue = balance - Number(floatValue);
+    newBalance = calculatedValue.toFixed(2);
+    income = true;
+    titleCat = title;
+  } else {
+    return;
   }
 
   if (newBalance < 0) {
     return null;
   }
 
-  const { title } = await Category.findById(idCategory);
 
   await User.findByIdAndUpdate(_id, { balance: newBalance }, { new: true });
 
   const transaction = await Transaction.create({
     ...body,
-    categories: title,
+    categories: titleCat,
     value: floatValue,
     income,
     owner: _id,
