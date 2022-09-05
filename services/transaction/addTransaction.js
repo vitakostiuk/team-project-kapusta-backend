38d3e735a49c04efd26f4cffc17077a60cdbbd04
unknown <vitagrebennik@gmail.com>
2022-09-05 12:56:31 +0300

    fixed balance

--- services/transaction/addTransaction.js
+++ services/transaction/addTransaction.js
@@ -1,40 +1,46 @@
 const { Transaction, User, Category } = require("../../models");
-const { tryCatchWrapper } = require('../../helpers');
+const { tryCatchWrapper } = require("../../helpers");
 
 const addTransaction = tryCatchWrapper(async ({ _id, body, type }) => {
-    const { balance } = await User.findById(_id);
-    const { value, categories: idCategory } = body;
+  const { balance } = await User.findById(_id);
+  const { value, categories: idCategory } = body;
 
-    let newBalance, income, calculatedValue;
-    const valueToNumber = Number(value);
-    const floatValue = valueToNumber.toFixed(2);
+  let newBalance, income, calculatedValue;
+  const valueToNumber = Number(value);
+  const floatValue = valueToNumber.toFixed(2);
 
-    switch (type){
-        case 'income':
-            calculatedValue = balance + Number(floatValue);
-            newBalance = calculatedValue.toFixed(2);
-            income = true;
-            break;
-        case 'expense':
-            calculatedValue = balance + Number(floatValue);
-            newBalance = calculatedValue.toFixed(2);
-            income = false;
-            break;
-        default:
-            return;
-    }
+  switch (type) {
+    case "income":
+      calculatedValue = balance + Number(floatValue);
+      newBalance = calculatedValue.toFixed(2);
+      income = true;
+      break;
+    case "expense":
+      calculatedValue = balance - Number(floatValue);
+      newBalance = calculatedValue.toFixed(2);
+      income = false;
+      break;
+    default:
+      return;
+  }
 
-    if (newBalance < 0) {
-        return null; 
-    }
+  if (newBalance < 0) {
+    return null;
+  }
 
-    const { title } = await Category.findById(idCategory);
- 
-    await User.findByIdAndUpdate(_id, { balance: newBalance }, { new: true });
+  const { title } = await Category.findById(idCategory);
 
-    const transaction = await Transaction.create({ ...body, categories: title, value: floatValue, income, owner: _id });
+  await User.findByIdAndUpdate(_id, { balance: newBalance }, { new: true });
 
-    return transaction;
+  const transaction = await Transaction.create({
+    ...body,
+    categories: title,
+    value: floatValue,
+    income,
+    owner: _id,
+  });
+
+  return transaction;
 });
 
 module.exports = addTransaction;
