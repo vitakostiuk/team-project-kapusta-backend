21097de8387ffe6a56b1e7a42706c7fc43ab2824
zpengineer <clickprime94@gmail.com>
2022-09-06 18:03:47 +0300

    Fixed add services bags

--- services/transaction/getTransaction.js
+++ services/transaction/getTransaction.js
@@ -3,13 +3,6 @@ const { Transaction } = require('../../models');
 
 const getTransaction = tryCatchWrapper(async ({userId, type, day, month, year}) => {
     let income;
-    // const nowDate = new Date();
-
-    // const date = {
-    //     day: String(nowDate.getDate()),
-    //     month: String(nowDate.getMonth() + 1),
-    //     year: String(nowDate.getFullYear())
-    // }
 
     const date = {
         day,
@@ -26,7 +19,8 @@ const getTransaction = tryCatchWrapper(async ({userId, type, day, month, year}) 
 
     const transaction = await Transaction
         .find({ income: income, owner: userId, date: date })
-        .sort({createdAt: -1});
+        .sort({ createdAt: -1 });
+    
     
     if (!transaction) {
         return null;
