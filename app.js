f118efe2eaa86a1c7a8f14a15a5c90313d5bb3fe
Nataliia Chapai <natalya.chapay@gmail.com>
2022-09-07 15:04:16 +0300

    clean project

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,11 +1,9 @@
 const validation = require("./validation");
 const isValidId = require("./isValidId");
 const user = require('./user');
-const upload = require('./upload');
 
 module.exports = {
     validation,
     isValidId,
-    user,
-    upload
+    user
 }
