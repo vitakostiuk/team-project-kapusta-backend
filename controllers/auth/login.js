3d9efa3269263cb04ec5524fde293294b9d1f541
Nataliia Chapai <natalya.chapay@gmail.com>
2022-09-12 19:10:26 +0300

    add date to response

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -25,13 +25,16 @@ const login = async(req, res) => {
         id: user._id
     };
     const token = jwt.sign(payload, SECRET_KEY, {expiresIn: '12h'});
+    const {createdAt, avatarURL} = user;
     await User.findByIdAndUpdate(user._id, {token})
     res.json({
         status: 'success',
         code: 200,
         token,
         user: {
-            email
+            email,
+            createdAt,
+            avatarURL
         }
     })
 }
